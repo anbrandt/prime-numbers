8c3729beb1fa54a53b92164d876a65a38637d32b
anbrandt <andrzej.brandt@gmail.com>
2017-05-18 17:37:47 +0200

    hello world

--- src/com/andrzej/AnotherPrimeMethod.java
+++ src/com/andrzej/AnotherPrimeMethod.java
@@ -4,4 +4,9 @@ package com.andrzej;
  * Created by andrzej on 18.05.17.
  */
 public class AnotherPrimeMethod {
+
+	public static void main(String[] args) {
+		System.out.println("Hi World");
+	}
+
 }
